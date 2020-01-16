ad6a37139ff05ea63ea93595613f1db9f1ec7e31
EGVinkel <emilvinkel@gmail.com>
2020-01-16 15:34:37 +0100

    fixed failing tests, due to methos being deleted

--- src/test/java/ws18/TokenManagementApplicationTest.java
+++ src/test/java/ws18/TokenManagementApplicationTest.java
@@ -6,7 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 public class TokenManagementApplicationTest {
     @Test
-    void contextLoads() {
+    public void contextLoads() {
 
     }
 }
--- src/test/java/ws18/controllers/TokenControllerTest.java
+++ src/test/java/ws18/controllers/TokenControllerTest.java
@@ -1,24 +1,12 @@
 package ws18.controllers;
 
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
-import ws18.exceptions.TooManyTokensException;
-import ws18.model.Token;
-import ws18.service.TokenManager;
 
-import java.util.ArrayList;
-
-import static org.hamcrest.core.StringContains.containsString;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -41,18 +29,6 @@ class TokenControllerTest {
     @Test
     void generateTokens() throws Exception {
         mockMvc.perform(post("/tokens/" + cpr))
-                .andExpect(status().isOk());
-    }
-
-    @Test
-    void getUnusedTokensByCpr() throws Exception {
-        mockMvc.perform(get("/tokens/"+cpr+"/unused"))
-                .andExpect(status().isOk());
-    }
-
-    @Test
-    void deleteTokens() throws Exception {
-        mockMvc.perform(delete("/tokens/"+cpr))
                 .andExpect(status().isOk());
     }
 
