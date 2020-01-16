73af4d4e001134e6986f8ef53291c6be3120a98a
Oliver <oliverskoeppen@gmail.com>
2020-01-16 10:05:12 +0100

    added generate tokens and get tokens

--- src/main/java/ws18/controllers/TokenController.java
+++ src/main/java/ws18/controllers/TokenController.java
@@ -1,6 +1,5 @@
 package ws18.controllers;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,25 +18,26 @@ import java.util.ArrayList;
 @RestController
 public class TokenController {
 
-    TokenManager tokenManager = new TokenManager();
+    private TokenManager tokenManager = new TokenManager();
 
     @RequestMapping("/token")
     public String index() {
         return "Greetings from tokencontroller";
     }
 
-    @RequestMapping(value = "/tokens/{cpr}", method = RequestMethod.GET)
+
+    @RequestMapping(path = "/tokens/{cpr}", method = RequestMethod.GET)
     public ArrayList<Token> getTokensByCpr(@PathVariable @NotNull String cpr) {
         return tokenManager.getTokensByCpr(cpr);
     }
 
-    @RequestMapping(value = "tokens/{cpr}/{amount}", method = RequestMethod.PUT)
-    public void generateTokens(@PathVariable @NotNull String cpr,
-                               @PathVariable @DecimalMax("6") @DecimalMin("0") int amount) {
-        try {
-            tokenManager.generateTokens(cpr, amount);
-        } catch (TooManyTokensException e) {
-            System.out.println(e);
+    @RequestMapping(path = "tokens/{cpr}/{amount}", method = RequestMethod.POST)
+    public ResponseEntity<Object> generateTokens(@PathVariable @NotNull String cpr,
+                                                 @PathVariable @DecimalMax("6") @DecimalMin("0") int amount) throws TooManyTokensException {
+        if (cpr == null || amount < 0 || amount > 6) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
+        tokenManager.generateTokens(cpr, amount);
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 }
--- src/test/java/ws18/controllers/TokenControllerTest.java
+++ src/test/java/ws18/controllers/TokenControllerTest.java
@@ -4,39 +4,65 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import ws18.exceptions.TooManyTokensException;
 import ws18.model.Token;
 import ws18.service.TokenManager;
 
 import java.util.ArrayList;
 
+import static org.hamcrest.core.StringContains.containsString;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@SpringBootTest
+@AutoConfigureMockMvc
 class TokenControllerTest {
 
     private TokenController tokenController = new TokenController();
-    String cpr = "112233-4455";
-    ArrayList<Token> tokens;
+    private String cpr = "112233-4455";
+    private int tokenAmount;
 
-    @After
-    void tearDown() {
-        tokenController = null;
-    }
+    @Autowired
+    private MockMvc mockMvc;
 
     @Test
-    void getTokens() {
-        tokens = tokenController.getTokensByCpr(cpr);
-        Assert.assertEquals(0,tokens.size());
-    }
-
-    @Test
-    void generateTokens() {
-        tokenController.generateTokens(cpr, 6);
-        tokens = tokenController.getTokensByCpr(cpr);
-        Assert.assertEquals(6, tokens.size());
-    }
-
-    @Test
-    void tooManyTokens() {
+    void tooManyTokens() throws TooManyTokensException {
         tokenController.generateTokens(cpr, 7);
     }
+
+    @Test
+    void getTokensByCpr() throws Exception {
+        mockMvc.perform(get("/tokens/" + cpr))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void generateTokens() throws Exception {
+        tokenAmount = 5;
+        mockMvc.perform(post("/tokens/" + cpr + "/" + tokenAmount))
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    void generateNegativeTokens() throws Exception {
+        tokenAmount = -1;
+        mockMvc.perform(post("/tokens/" + cpr + "/" + tokenAmount))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    void generateTooManyTokens() throws Exception {
+        tokenAmount = 7;
+        mockMvc.perform(post("/tokens/" + cpr + "/" + tokenAmount))
+                .andExpect(status().isBadRequest());
+    }
+
 }
