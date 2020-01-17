245f95bfc63f62613e67d2de772ecbf576ca8f1f
EGVinkel <emilvinkel@gmail.com>
2020-01-17 18:12:43 +0100

    Changed message queueing

---
+++ src/main/java/configuration/RabbitMQValues.java
@@ -0,0 +1,11 @@
+package configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQValues {
+    public static final String topicExchangeName = "dtupay-eventsExchange";
+
+
+
+}
--- src/main/java/ws18/TokenManagementApplication.java
+++ src/main/java/ws18/TokenManagementApplication.java
@@ -1,62 +1,33 @@
 package ws18;
 
-import org.springframework.amqp.core.Binding;
-import org.springframework.amqp.core.BindingBuilder;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
-import org.springframework.amqp.rabbit.connection.ConnectionFactory;
-import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
-import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
+import configuration.RabbitMQValues;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
-import ws18.rabbitmq.Receiver;
 
 @EnableSwagger2
 @SpringBootApplication
 public class TokenManagementApplication {
-    public static final String topicExchangeName = "dtupay-eventsExchange";
+    private static final String queueName = "token-queue";
+    private RabbitTemplate rabbitTemplate;
 
-    public static final String queueName = "dtupay-queue";
-
-    @Bean
-    Queue queue() {
-        return new Queue(queueName, false);
-    }
-
-    @Bean
-    TopicExchange exchange() {
-        return new TopicExchange(topicExchangeName);
-    }
-
-    @Bean
-    Binding binding(Queue queue, TopicExchange exchange) {
-        return BindingBuilder.bind(queue).to(exchange).with("token");
-    }
-
-    @Bean
-    SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
-                                             MessageListenerAdapter listenerAdapter) {
-        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
-        container.setConnectionFactory(connectionFactory);
-        container.setQueueNames(queueName);
-        container.setMessageListener(listenerAdapter);
-        return container;
-    }
-
-    @Bean
-    MessageListenerAdapter listenerAdapter(Receiver receiver) {
-        return new MessageListenerAdapter(receiver, "receiveMessage");
+    @Autowired
+    public TokenManagementApplication(RabbitTemplate rabbitTemplate){
+        this.rabbitTemplate = rabbitTemplate;
     }
 
 
+
+    @RabbitListener(queues = {queueName})
+    public void receiveMessageFromFanout1(String message) {
+        System.out.println("Token manager received message: " + message);
+        this.rabbitTemplate.convertAndSend(RabbitMQValues.topicExchangeName, "response", "Completed");
+    }
     public static void main(String[] args) {
-
-
-
         SpringApplication.run(TokenManagementApplication.class, args);
-
     }
-    }
+}
 
