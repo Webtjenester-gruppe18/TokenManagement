71d9c2529c426fb81b5a178316e409e45a4af202
Marcus August Christiansen <s175185@student.dtu.dk>
2020-01-18 20:50:42 +0100

    Moved listener to Listener

--- src/main/java/ws18/TokenManagementApplication.java
+++ src/main/java/ws18/TokenManagementApplication.java
@@ -11,21 +11,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 @SpringBootApplication
 public class TokenManagementApplication {
-    private static final String queueName = "token-queue";
-    private RabbitTemplate rabbitTemplate;
-
-    @Autowired
-    public TokenManagementApplication(RabbitTemplate rabbitTemplate){
-        this.rabbitTemplate = rabbitTemplate;
-    }
-
-
-
-    @RabbitListener(queues = {queueName})
-    public void receiveMessageFromFanout1(String message) {
-        System.out.println("Token manager received message: " + message);
-        this.rabbitTemplate.convertAndSend(RabbitMQValues.topicExchangeName, "response", "Completed");
-    }
     public static void main(String[] args) {
         SpringApplication.run(TokenManagementApplication.class, args);
     }
---
+++ src/main/java/ws18/listeners/Listeners.java
@@ -0,0 +1,37 @@
+package ws18.listeners;
+
+import configuration.RabbitMQValues;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import ws18.exceptions.TooManyTokensException;
+import ws18.service.ITokenManager;
+
+@Service
+public class Listeners {
+
+    private static final String queueName = "token-queue";
+    private RabbitTemplate rabbitTemplate;
+    private ITokenManager tokenManager;
+
+    @Autowired
+    public Listeners(RabbitTemplate rabbitTemplate, ITokenManager tokenManager){
+        this.rabbitTemplate = rabbitTemplate;
+        this.tokenManager = tokenManager;
+    }
+
+    @RabbitListener(queues = {queueName})
+    public void receiveEventFromTokenQueue(String message) {
+        System.out.println("Token manager received message: " + message);
+        if (event.getType().equals(REQUEST_TOKEN)) {
+            try {
+                this.tokenManager.requestForNewTokens("basd");
+            } catch (TooManyTokensException e) {
+
+            }
+        }
+
+        this.rabbitTemplate.convertAndSend(RabbitMQValues.topicExchangeName, "response", "Completed");
+    }
+}
