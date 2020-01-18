1a346da6df8e7243d395888eef027f8719a33a93
EGVinkel <emilvinkel@gmail.com>
2020-01-18 23:53:01 +0100

    Structural changes

--- src/main/java/ws18/TokenManagementApplication.java
+++ src/main/java/ws18/TokenManagementApplication.java
@@ -1,9 +1,5 @@
 package ws18;
 
-import configuration.RabbitMQValues;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
