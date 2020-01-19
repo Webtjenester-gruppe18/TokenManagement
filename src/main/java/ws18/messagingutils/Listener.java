d7b4ef9e974f5c2e3a8aabf2c2a42c76ab263e69
Marcus August Christiansen <s175185@student.dtu.dk>
2020-01-19 13:42:22 +0100

    Request new tokens

--- src/main/java/ws18/messagingutils/Listener.java
+++ src/main/java/ws18/messagingutils/Listener.java
@@ -6,6 +6,7 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ws18.exceptions.TokenValidationException;
+import ws18.exceptions.TooManyTokensException;
 import ws18.model.Event;
 import ws18.model.EventType;
 import ws18.model.PaymentRequest;
@@ -27,6 +28,7 @@ public class Listener {
     @RabbitListener(queues = {RabbitMQValues.TOKEN_SERVICE_QUEUE_NAME})
     public void receiveEventFromTokenQueue(Event event) {
         System.out.println("Token manager received message: " + event.getType());
+
         if (event.getType().equals(EventType.TOKEN_VALIDATION_REQUEST)) {
             PaymentRequest paymentRequest = mapper.convertValue(event.getObject(), PaymentRequest.class);
 
@@ -39,6 +41,20 @@ public class Listener {
 
             event.setType(EventType.MONEY_TRANSFER_REQUEST);
             this.rabbitTemplate.convertAndSend(RabbitMQValues.TOPIC_EXCHANGE_NAME, RabbitMQValues.PAYMENT_SERVICE_ROUTING_KEY, event);
+        }
+        else if (event.getType().equals(EventType.REQUEST_FOR_NEW_TOKENS)) {
+            String cpr = mapper.convertValue(event.getObject(), String.class);
+
+            try {
+                this.tokenManager.requestForNewTokens(cpr);
+            } catch (TooManyTokensException e) {
+                Event response = new Event(EventType.TOKEN_GENERATION_FAILED, e);
+                this.rabbitTemplate.convertAndSend(RabbitMQValues.TOPIC_EXCHANGE_NAME, RabbitMQValues.DTU_SERVICE_ROUTING_KEY, response);
+                return;
+            }
+
+            Event successResponse = new Event(EventType.TOKEN_GENERATION_SUCCEED, EventType.TOKEN_GENERATION_SUCCEED);
+            this.rabbitTemplate.convertAndSend(RabbitMQValues.TOPIC_EXCHANGE_NAME, RabbitMQValues.DTU_SERVICE_ROUTING_KEY, successResponse);
         }
     }
 }
